50908d4ca04c50a1e42b635ff8ced98eebb22999
David McCullough <dmccullough488@gmail.com>
2025-09-20 11:58:00 -0500

    add draw cards btn and handle draw function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let deckId = null;
+let newCards = []
 
 function handleClick() {
   fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
@@ -10,5 +11,16 @@ function handleClick() {
     })
 }
 
+function handleDraw() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      newCards = data.cards
+      console.log('Drew cards:', newCards)
+    })
+}
+
 document.getElementById('new-deck').addEventListener('click', handleClick)
+document.getElementById('draw-cards').addEventListener('click', handleDraw)
 
